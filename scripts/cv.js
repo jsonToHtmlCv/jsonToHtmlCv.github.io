e304cb517e1c1608df40973fd771171b92dc8879
Smokli <kamen.davidkov@gmail.com>
2015-05-30 18:27:15 -0700

    First commit

---
+++ scripts/cv.js
@@ -0,0 +1,110 @@
+﻿var cv = [
+{
+    "Personal Details": [
+        { "First name": "Kamen" },
+        { "Middle name": "Lubomirov" },
+        { "Last name": "Davidkov" },
+        { "Gender": "Male" },
+        { "Birth date": "16.11.1989" },
+        { "Nationality": "Bulgaria" },
+        { "I live in": "Sofia/Bulgaria" }
+    ]
+},
+{
+    "Contacts": [
+    { "Email": "kamen.davidkov@gmail.com" },
+    { "Phone": "0883 303 993" },
+    { "Post address": "Sofia, 59 Kalimatsi St." }
+    ]
+},
+{
+    "Work Experience": [
+    [
+        { "Period": "August 2014 - May 2015" },
+        { "Job Title": "HTML5/JavaScript Developer" },
+        { "Company/Organization": "Plus Minus Ltd." },
+        { "Business sector": "Information technologies" },
+        { "Location": "Sofia" },
+        { "Company size": "20-50" }
+    ],
+    [
+        { "Period": "September 2011 - August 2014" },
+        { "Job Title": "Administrative and IT assistant" },
+        { "Company/Organization": "Debora 33" },
+        { "Business sector": "Textile industry" },
+        { "Location": "Sofia" },
+        { "Company size": "5-10" }
+    ],
+    [
+        { "Period": "April 2009 - August 2009" },
+        { "Job Title": "Archeology team leader" },
+        { "Company/Organization": "Bulgarian Academy of Science" },
+        { "Business sector": "Education, science" },
+        { "Location": "Bulgaria, Sofia" },
+        { "Company size": "above 300" }
+    ]
+    ]
+},
+{
+    "Education": [
+    [
+        { "Period": "March 2014 - till now" },
+        { "Major": "Web" },
+        { "Level": "Professional" },
+        { "School": "Telerik Academy" },
+        { "Location": "Bulgaria, Sofia" },
+        { "Description/Main Subjects": "C# част II, C# ООП, HTML, CSS, JavaScript: Fundamentals/ UI & DOM/ OOP" }
+    ],
+    [
+        { "Period": "October 2011 - till now" },
+        { "Major": "Philosophy" },
+        { "Level": "Bachelor's degree" },
+        { "School": "Sofia University" },
+        { "Location": "Sofia / България" }
+    ],
+    [
+        { "Period": "October 2008 - February 2010" },
+        { "Major": "Archeology" },
+        { "Level": "Bachelor's degree" },
+        { "School": "New Bulgarian University" },
+        { "Location": "Bulgaria, Sofia" }
+    ],
+    [
+        { "Period": "September 1996 - May 2008" },
+        { "Major": "Languages and history" },
+        { "Level": "Secondary school" },
+        { "School": "8th \"Vasil Levski\" School" },
+        { "Location": "Bulgaria, Sofia" }
+    ]
+    ]
+},
+{
+    "Languages": [
+        {
+            "Mother tongue": "Bulgarian"
+        },
+        {
+            "English": [
+                { "Comprehension": "Intermediate" },
+                { "Speaking": "Intermediate" },
+                { "Writing": "Intermediate" }
+            ]
+        }
+    ]
+},
+{
+    "Skills": [
+        { "Computer Skills": "C#, C# ООП, HTML, CSS and JavaScript" },
+        { "Skills": "Strong research skills; Flexible thinker and team-worker; Works well under pressure and strict deadlines;" },
+        { "Managed people": "Yes (25)" },
+        { "Driving License": "Yes" },
+        { "Categories": "B" }
+    ]
+},
+{
+    "Additional Courses and Certificates": [
+        { "Additional Courses": "C# part II; C# ООП; HTML basics; CSS Styling (and SASS); JavaScript: Fundamentals/ UI and DOM (jQuery, Handlebars)/ OOP" },
+        { "Certificates": "Cambridge ESOL: First Certificate in English (FCE)" }
+    ]
+}
+];
---
+++ scripts/script.js
@@ -0,0 +1,62 @@
+﻿
+
+function buildCV(cv) {
+
+    var docFrag = document.createDocumentFragment(),
+        header = document.createElement('h3'),
+        div = document.createElement('div'),
+        span = document.createElement('span');
+    header.style.borderBottom = '1px solid #d5d5d5';
+    header.style.padding = '0px 5px';
+    header.style.margin = '5px 0px';
+    span.style.margin = '0px 3px';
+    span.style.display = 'inline-block';
+    div.style.padding = '3px 3px 3px 10px';
+    var firstContainer = div.cloneNode(true),
+        headingWrapper,
+        prop,
+        value;
+    docFrag.appendChild(firstContainer);
+    recursiveBuild(cv, firstContainer);
+
+    function recursiveBuild(cvObj, oldContainer) {
+        if (Array.isArray(cvObj)) {
+            for (var a = 0, len = cvObj.length; a < len; a++) {
+                var newContainer = div.cloneNode(true);
+                oldContainer.appendChild(newContainer);
+                recursiveBuild(cvObj[a], newContainer);
+            }
+        }
+        else if (Object.prototype.toString.call(cvObj) === '[object Object]') {
+
+            for (var word in cvObj) {
+                if (Array.isArray(cvObj[word])) {
+                    headingWrapper = header.cloneNode(true);
+                    headingWrapper.innerHTML = word;
+                    oldContainer.appendChild(headingWrapper);
+                    oldContainer.style.border = '1px solid #d5d5d5';
+                    var newContainer = div.cloneNode(true);
+                    oldContainer.appendChild(newContainer);
+                    recursiveBuild(cvObj[word], newContainer);
+                }
+                else {
+                    prop = span.cloneNode(true);
+                    value = span.cloneNode(true);
+                    prop.innerHTML = word + ': ';
+                    prop.style.textAlign = 'right';
+                    prop.style.width = '20%';
+                    value.innerHTML = cvObj[word];
+                    value.style.textAlign = 'left';
+                    oldContainer.appendChild(prop);
+                    oldContainer.appendChild(value);
+                }
+            }
+        }
+    }
+
+    document.body.appendChild(docFrag);
+}
+
+buildCV(cv);
+
+
